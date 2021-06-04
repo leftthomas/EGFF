7f4449cb82cc296e40eb94d1ad5f3e99e6f48aac
leftthomas <leftthomas@qq.com>
2021-06-04 16:11:23 +0800

    finish model with two backbones

--- model.py
+++ model.py
@@ -1,47 +1,29 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet50
+from torchvision.models import resnet50, vgg16
 
 
 class Model(nn.Module):
-    def __init__(self, proj_dim):
+    def __init__(self, backbone_type, proj_dim):
         super(Model, self).__init__()
         # encoder
-        self.f = []
-        for name, module in resnet50(pretrained=True).named_children():
-            if not isinstance(module, nn.Linear):
-                self.f.append(module)
-        # encoder
-        self.f = nn.Sequential(*self.f)
-        # projection head
-        self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
+        if backbone_type == 'resnet50':
+            self.f = []
+            for module in resnet50(pretrained=True).children():
+                if not isinstance(module, (nn.Linear, nn.AdaptiveAvgPool2d)):
+                    self.f.append(module)
+            self.f = nn.Sequential(*self.f)
+        elif backbone_type == 'vgg16':
+            self.f = vgg16(pretrained=True).features
+        else:
+            raise NotImplementedError('Not support {} as backbone'.format(backbone_type))
+        # head
+        in_channel = 2048 if backbone_type == 'resnet50' else 512
+        self.g = nn.Sequential(nn.Linear(in_channel, 512, bias=False), nn.BatchNorm1d(512),
                                nn.ReLU(inplace=True), nn.Linear(512, proj_dim, bias=True))
 
     def forward(self, x):
-        feature = torch.flatten(self.f(x), start_dim=1)
+        feature = torch.flatten(F.adaptive_avg_pool2d(self.f(x), output_size=(1, 1)), start_dim=1)
         proj = self.g(feature)
-        return F.normalize(feature, dim=-1), F.normalize(proj, dim=-1)
-
-
-class SimCLRLoss(nn.Module):
-    def __init__(self, temperature):
-        super(SimCLRLoss, self).__init__()
-        self.temperature = temperature
-
-    def forward(self, proj_1, proj_2):
-        batch_size = proj_1.size(0)
-        # [2*B, Dim]
-        out = torch.cat([proj_1, proj_2], dim=0)
-        # [2*B, 2*B]
-        sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / self.temperature)
-        mask = (torch.ones_like(sim_matrix) - torch.eye(2 * batch_size, device=sim_matrix.device)).bool()
-        # [2*B, 2*B-1]
-        sim_matrix = sim_matrix.masked_select(mask).view(2 * batch_size, -1)
-
-        # compute loss
-        pos_sim = torch.exp(torch.sum(proj_1 * proj_2, dim=-1) / self.temperature)
-        # [2*B]
-        pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
-        loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
-        return loss
+        return F.normalize(proj, dim=-1)
