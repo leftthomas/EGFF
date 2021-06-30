365354145a110a48bd7bd47c060e9f11464b8f6e
leftthomas <leftthomas@qq.com>
2021-06-30 09:04:03 +0800

    avoid loss nan

--- model.py
+++ model.py
@@ -6,12 +6,13 @@ from torchvision.models import resnet50, vgg16
 
 
 class GeM(nn.Module):
-    def __init__(self, p=3):
+    def __init__(self, p=3, eps=1e-6):
         super(GeM, self).__init__()
         self.p = nn.Parameter(torch.ones(1) * p)
+        self.eps = eps
 
     def forward(self, x):
-        return F.avg_pool2d(x.pow(self.p), (x.size(-2), x.size(-1))).pow(1.0 / self.p)
+        return F.avg_pool2d(x.clamp(min=self.eps).pow(self.p), (x.size(-2), x.size(-1))).pow(1.0 / self.p)
 
 
 class SEBlock(nn.Module):
