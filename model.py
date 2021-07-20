27a026997ed443c97f21dd4e9408cddf25f1b5f4
leftthomas <leftthomas@qq.com>
2021-07-20 18:10:36 +0800

    add cbam

--- model.py
+++ model.py
@@ -5,15 +5,31 @@ from kornia.filters import sobel
 from torchvision.models import resnet50, vgg16
 
 
+class CBAM(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(CBAM, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False))
+
+    def forward(self, x):
+        x_avg = self.fc(F.adaptive_avg_pool2d(x, (1, 1)))
+        x_max = self.fc(F.adaptive_max_pool2d(x, (1, 1)))
+        y = torch.sigmoid(x_avg + x_max)
+        return x * y
+
+
 class GlobalContext(nn.Module):
-    def __init__(self):
+    def __init__(self, channel, reduction=16):
         super(GlobalContext, self).__init__()
-        self.conv_attn = nn.Conv2d(3, 1, 1, bias=True)
+        self.cbam = CBAM(channel, reduction)
+        self.attn = nn.Conv2d(3, 1, 1, bias=True)
 
     def forward(self, x, edge):
         edge = F.interpolate(edge, size=x.size()[-2:], mode='bilinear', align_corners=False)
-        attn = torch.sigmoid(self.conv_attn(edge))
-        context = attn * x
+        attn = torch.sigmoid(self.attn(edge))
+        context = attn * self.cbam(x)
         return context
 
 
@@ -36,21 +52,25 @@ class Model(nn.Module):
         else:
             raise NotImplementedError('Not support {} as backbone'.format(backbone_type))
         # atte and proj
-        self.low_atte = GlobalContext()
-        self.middle_atte = GlobalContext()
-        self.high_atte = GlobalContext()
-        self.proj = nn.Linear(2048 if backbone_type == 'resnet50' else 512, proj_dim)
+        self.low_atte = GlobalContext(256)
+        self.middle_atte = GlobalContext(512)
+        self.high_atte = GlobalContext(2048 if backbone_type == 'resnet50' else 512)
+        self.proj = nn.Linear(256 + 512 + 2048 if backbone_type == 'resnet50' else 256 + 512 + 512, proj_dim)
         self.backbone_type = backbone_type
 
     def forward(self, img):
         edge = sobel(img)
         low_feat = self.feat(img)
-        low_feat = self.low_atte(low_feat, edge)
         middle_feat = self.common[:1 if self.backbone_type == 'resnet50' else 7](low_feat)
-        middle_feat = self.middle_atte(middle_feat, edge)
         high_feat = self.common[1 if self.backbone_type == 'resnet50' else 7:](middle_feat)
-        high_feat = self.high_atte(high_feat, edge)
 
-        feat = torch.flatten(F.adaptive_max_pool2d(high_feat, (1, 1)), start_dim=1)
+        atte_low_feat = self.low_atte(low_feat, edge)
+        atte_middle_feat = self.middle_atte(middle_feat, edge)
+        atte_high_feat = self.high_atte(high_feat, edge)
+
+        low_feat = torch.flatten(F.adaptive_max_pool2d(atte_low_feat, (1, 1)), start_dim=1)
+        middle_feat = torch.flatten(F.adaptive_max_pool2d(atte_middle_feat, (1, 1)), start_dim=1)
+        high_feat = torch.flatten(F.adaptive_max_pool2d(atte_high_feat, (1, 1)), start_dim=1)
+        feat = torch.cat((low_feat, middle_feat, high_feat), dim=-1)
         proj = self.proj(feat)
         return F.normalize(proj, dim=-1)
