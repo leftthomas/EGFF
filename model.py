47042e22c41aa2178d71d0f1408a03d6c54c6829
leftthomas <leftthomas@qq.com>
2021-07-20 11:50:44 +0800

    remove cbam

--- model.py
+++ model.py
@@ -1,53 +1,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from kornia.filters import sobel
 from torchvision.models import resnet50, vgg16
 
 
-class LayerNorm2d(nn.LayerNorm):
-    """ LayerNorm for channels of '2D' spatial BCHW tensors """
-
-    def __init__(self, num_channels):
-        super().__init__(num_channels)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return F.layer_norm(
-            x.permute(0, 2, 3, 1), self.normalized_shape, self.weight, self.bias, self.eps).permute(0, 3, 1, 2)
-
-
 class GlobalContext(nn.Module):
-    def __init__(self, channel, reduction=4):
+    def __init__(self):
         super(GlobalContext, self).__init__()
-        self.conv_attn = nn.Conv2d(channel, 1, 1, bias=True)
-        self.fc = nn.Sequential(
-            nn.Conv2d(channel, channel // reduction, 1, bias=True),
-            LayerNorm2d(channel // reduction),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channel // reduction, channel, 1, bias=True))
+        self.conv_attn = nn.Conv2d(3, 1, 1, bias=True)
 
-    def forward(self, x):
-        b, c, _, _ = x.shape
-        attn = self.conv_attn(x).reshape(b, 1, -1)
-        attn = F.softmax(attn, dim=-1).unsqueeze(3)
-        context = x.reshape(b, c, -1).unsqueeze(1) @ attn
-        context = context.view(b, c, 1, 1)
-        y = torch.sigmoid(self.fc(context))
-        return x * y
-
-
-class CBAM(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(CBAM, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Conv2d(channel, channel // reduction, 1, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channel // reduction, channel, 1, bias=False))
-
-    def forward(self, x):
-        x_avg = self.fc(F.adaptive_avg_pool2d(x, (1, 1)))
-        x_max = self.fc(F.adaptive_max_pool2d(x, (1, 1)))
-        y = torch.sigmoid(x_avg + x_max)
-        return x * y
+    def forward(self, x, edge):
+        edge = F.interpolate(edge, size=x.size()[-2:], mode='bilinear', align_corners=False)
+        attn = torch.sigmoid(self.conv_attn(edge))
+        context = attn * x
+        return context
 
 
 class Model(nn.Module):
@@ -69,19 +36,20 @@ class Model(nn.Module):
         else:
             raise NotImplementedError('Not support {} as backbone'.format(backbone_type))
         # atte and proj
-        self.low_atte = CBAM(256)
-        self.middle_atte = CBAM(512)
-        self.high_atte = CBAM(2048 if backbone_type == 'resnet50' else 512)
+        self.low_atte = GlobalContext()
+        self.middle_atte = GlobalContext()
+        self.high_atte = GlobalContext()
         self.proj = nn.Linear(2048 if backbone_type == 'resnet50' else 512, proj_dim)
         self.backbone_type = backbone_type
 
     def forward(self, img):
+        edge = sobel(img)
         low_feat = self.feat(img)
-        # low_feat = self.low_atte(low_feat)
+        low_feat = self.low_atte(low_feat, edge)
         middle_feat = self.common[:1 if self.backbone_type == 'resnet50' else 7](low_feat)
-        # middle_feat = self.middle_atte(middle_feat)
+        middle_feat = self.middle_atte(middle_feat, edge)
         high_feat = self.common[1 if self.backbone_type == 'resnet50' else 7:](middle_feat)
-        # high_feat = self.high_atte(high_feat)
+        high_feat = self.high_atte(high_feat, edge)
 
         feat = torch.flatten(F.adaptive_max_pool2d(high_feat, (1, 1)), start_dim=1)
         proj = self.proj(feat)
