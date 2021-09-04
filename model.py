2e2613b1b76f3150b63d4fa0dfe8727e14144826
leftthomas <leftthomas@qq.com>
2021-09-04 15:31:39 +0800

    only use three blocks

--- model.py
+++ model.py
@@ -36,28 +36,25 @@ class Model(nn.Module):
 
         # backbone
         self.backbone = timm.create_model('seresnet50' if backbone_type == 'resnet50' else 'vgg16_bn',
-                                          features_only=True, out_indices=(1, 2, 3, 4), pretrained=True)
-        dims = [256, 512, 1024, 2048] if backbone_type == 'resnet50' else [128, 256, 512, 512]
+                                          features_only=True, out_indices=(2, 3, 4), pretrained=True)
+        dims = [512, 1024, 2048] if backbone_type == 'resnet50' else [256, 512, 512]
 
         # atte
-        self.energy_1 = EnergyAttention(dims[0], dims[3])
-        self.energy_2 = EnergyAttention(dims[1], dims[3])
-        self.energy_3 = EnergyAttention(dims[2], dims[3])
+        self.energy_1 = EnergyAttention(dims[0], dims[2])
+        self.energy_2 = EnergyAttention(dims[1], dims[2])
 
         # proj
         self.proj = nn.Linear(sum(dims), proj_dim)
 
     def forward(self, img):
-        block_1_feat, block_2_feat, block_3_feat, block_4_feat = self.backbone(img)
-        block_1_atte, block_1_feat = self.energy_1(block_1_feat, block_4_feat)
-        block_2_atte, block_2_feat = self.energy_2(block_2_feat, block_4_feat)
-        block_3_atte, block_3_feat = self.energy_3(block_3_feat, block_4_feat)
+        block_1_feat, block_2_feat, block_3_feat = self.backbone(img)
+        block_1_atte, block_1_feat = self.energy_1(block_1_feat, block_3_feat)
+        block_2_atte, block_2_feat = self.energy_2(block_2_feat, block_3_feat)
 
         block_1_feat = torch.flatten(F.adaptive_max_pool2d(block_1_feat, (1, 1)), start_dim=1)
         block_2_feat = torch.flatten(F.adaptive_max_pool2d(block_2_feat, (1, 1)), start_dim=1)
         block_3_feat = torch.flatten(F.adaptive_max_pool2d(block_3_feat, (1, 1)), start_dim=1)
-        block_4_feat = torch.flatten(F.adaptive_max_pool2d(block_4_feat, (1, 1)), start_dim=1)
 
-        feat = torch.cat((block_1_feat, block_2_feat, block_3_feat, block_4_feat), dim=-1)
+        feat = torch.cat((block_1_feat, block_2_feat, block_3_feat), dim=-1)
         proj = self.proj(feat)
         return F.normalize(proj, dim=-1)
